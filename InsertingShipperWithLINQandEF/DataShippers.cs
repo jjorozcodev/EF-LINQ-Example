2268df4282951ec532461030e154dfa173eab895
JJ <jj.orozcolopez.13@gmail.com>
2019-09-07 06:57:43 -0600

    Shipper entity created and list method.

---
+++ InsertingShipperWithLINQandEF/DataShippers.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace InsertingShipperWithLINQandEF
+{
+    public class DataShippers
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        public List<Shipper> GetShippers()
+        {
+            return db.Shippers.ToList();
+        }
+    }
+}
--- InsertingShipperWithLINQandEF/Program.cs
+++ InsertingShipperWithLINQandEF/Program.cs
@@ -1,15 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InsertingShipperWithLINQandEF
 {
     class Program
     {
+        private static DataShippers dShippers = new DataShippers();
+
         static void Main(string[] args)
         {
+            char k = char.MinValue;
+            bool bContinue = false;
+
+            do
+            {
+                PrintShippers();
+                k = CatchKey();
+                bContinue = WantRegister(k);
+                if (bContinue)
+                {
+                    InsertShipper();
+                }
+            } while (bContinue);
+        }
+
+        private static void PrintShippers()
+        {
+            Console.Clear();
+
+            Console.WriteLine("++++++++++ NORTHWIND'S SHIPPERS ++++++++++");
+            Console.WriteLine();
+
+            List<Shipper> shippers = dShippers.GetShippers();
+
+            Console.WriteLine("--- ID ------- NAME ------- PHONE -------");
+
+            foreach (Shipper s in shippers)
+            {
+                Console.WriteLine("|  " + s.ShipperID + "   " + s.CompanyName + "    " + s.Phone + "  |");
+            }
+        }
+
+        private static char CatchKey()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press (R) to insert new Shipper or any key to quit...");
+            
+            return Console.ReadKey().KeyChar;
+        }
+
+        private static bool WantRegister(char k)
+        {
+            return (k.Equals('r') || k.Equals('R'));
+        }
+
+        private static void InsertShipper()
+        {
+            //Shipper s = new Shipper();
+
+            Console.Clear();
+
+            Console.WriteLine("++++++++++ NORTHWIND'S SHIPPERS ++++++++++");
+            Console.WriteLine();
+
+            Console.WriteLine("Please, insert new shipper here...");
+            Console.ReadKey();
         }
     }
 }
